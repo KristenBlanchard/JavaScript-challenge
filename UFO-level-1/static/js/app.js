79f7df8a4ba0a187dbb1c858150a5f140dccd756
Kristen <68074437+KristenBlanchard@users.noreply.github.com>
2020-11-02 08:43:01 -0500

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,8 +10,8 @@ var columns = ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes
 // Inpu data into HTML
 var appendData = (dataInput) => {
     dataInput.forEach(ufoSightings => {
-        var row = tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        var row = tbody.append('tr');
+        columns.forEach(column => row.append('td').text(ufoSightings[column])
         )
     })
 };
@@ -19,3 +19,25 @@ var appendData = (dataInput) => {
 appendData(tableData);
 
 // ------- Event listener --------
+
+button.on('click', () => {
+    d3.event.preventDefault();
+
+    var inputDatetime = inputFieldDate.property("value").trim();
+
+    var filterDatetime = tableData.filter(tableData => tableData.datetime === inputDatetime);
+    
+    tbody.html('');
+
+    let response = {
+        filterDatetime
+    }
+
+    if(response.filterDatetime.length !== 0) {
+        addData(filterDatetime);
+    }
+
+    else {
+        tbody.append('tr').append('td').text("No sightings...Choose Another....");
+    }
+})
